801f48ec16b141dca8ff9a38a4daeaac10d8eeb2
fdblay <67843349+fdblay@users.noreply.github.com>
2025-03-23 11:31:15 +0000

    complete game

--- battleship.js
+++ battleship.js
@@ -1,14 +1,207 @@
+const view = {
+    displayMessage: function (msg) {
+        let messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
 
-function addSongs() {
-    let song1 = document.getElementById("song1");
-    let song2 = document.getElementById("song2");
-    let song3 = document.getElementById("song3");
+    },
 
-    song1.innerHTML = "Blue Suede Strings, by Elvis Pagely";
-    song2.innerHTML = "Great Objects on Fire, by Jerry JSON Lewis";
-    song3.innerHTML = "I Code the Line, by Freddy JavaScript";
+    displayHit: function (location) {
+        let cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+    },
 
-    song1.setAttribute("class", "bluetext");
+    displayMiss: function (location) {
+        let cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
+    },
+};
+
+function parseGuess(guess) {
+    let alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+    if (guess === null || guess.length !== 2) {
+        alert("Oops, please enter a letter and a number on the board.");
+    } else {
+        firstChar = guess.charAt(0);
+        let row = alphabet.indexOf(firstChar);
+        let column = guess.charAt(1);
+
+        if (isNaN(row) || isNaN(column)) {
+            alert("Oops, that isn't on the board.");
+        } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+            alert("Oops, that's off the board!");
+        } else {
+            return row + column;
+        }
+    }
+    return null;
+};
+
+
+
+
+let model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+
+    ships: [
+        { locations: [0, 0, 0], hits: ["", "", ""] },
+        { locations: [0, 0, 0], hits: ["", "", ""] },
+        { locations: [0, 0, 0], hits: ["", "", ""] }
+    ],
+
+    fire: function (guess) {
+        for (let i = 0; i < this.numShips; i++) {
+            let ship = this.ships[i];
+            let guessIndex = ship.locations.indexOf(guess);
+
+            if (guessIndex >= 0) {
+                // we have a hit! else a miss!
+                ship.hits[guessIndex] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
+                if (this.isSunk(ship)) {
+                    view.displayMessage("You sunk my battleship!");
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage("You MISSED.");
+        return false;
+    },
+
+    isSunk: function (ship) {
+        for (let i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit") {
+                return false;
+            }
+        }
+        return true;
+    },
+
+
+    generateShipLocations: function() {
+        let locations;
+
+        for (let i = 0; i < this.numShips; i++) {
+            do {
+                locations = this.generateShip();
+            } while (this.collision(locations));
+            this.ships[i].locations = locations;
+        }
+    },
+
+    generateShip: function() {
+        let direction = Math.floor(Math.random() * 2);
+        let row, col;
+
+        if (direction === 1) {
+            row = Math.floor(Math.random() * this.boardSize);
+            col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+        } else {
+            row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+            col = Math.floor(Math.random() * this.boardSize);
+        }
+
+        let newShipLocations = [];
+        for (let i = 0; i < this.shipLength; i++) {
+            if (direction === 1) {
+                newShipLocations.push(row + "" + (col + i));
+            } else {
+                newShipLocations.push((row + i) + "" + col);
+            }
+        }
+        return newShipLocations;
+    },
+
+    collision: function(locations) {
+        for (let i = 0; i < this.numShips; i++) {
+            let ship = model.ships[i];
+            for (let j = 0; j < locations.length; j++) {
+                if (ship.locations.indexOf(locations[j]) >= 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+   
+};
+
+let controller = {
+    guesses: 0,
+
+    processGuess: function (guess) {
+
+        let location = parseGuess(guess);
+        if (location) {
+            this.guesses++;
+            let hit = model.fire(location);
+            if (hit && model.shipsSunk === model.numShips) {
+                view.displayMessage("You sank all my battleships, in " + this.guesses + " guesses");
+            }
+        }
+        
+    }
+};
+
+
+function init() {
+    let fireButton = document.getElementById("fireButton");
+    fireButton.onclick = handleFireButton; 
+    
+    let guessInput = document.getElementById("guessInput");
+    guessInput.onkeypress = handleKeyPress;
+
+    model.generateShipLocations();
+    
+};
+
+function handleKeyPress(e) {
+    let fireButton = document.getElementById("fireButton");
+    
+    if (e.keyCode === 13) {
+        fireButton.click();
+        return false;
+    }
 }
-addSongs();
-// window.onload = addSongs;
+
+function handleFireButton() {
+    let guessInput = document.getElementById("guessInput");
+    let guess = guessInput.value;
+    
+    controller.processGuess(guess);
+    
+    guessInput.value = "";
+};
+
+window.onload = init;
+
+
+// controller.processGuess("A0");
+
+// controller.processGuess("A6");
+// controller.processGuess("B6");
+// controller.processGuess("C6");
+
+// model.fire("24");
+// model.fire("34");
+// model.fire("44");
+
+// model.fire("12");
+// model.fire("11");
+// model.fire("10");
+
+
+// view.displayMiss("00");
+// view.displayHit("34");
+// view.displayMiss("55");
+// view.displayHit("12");
+// view.displayMiss("25");
+// view.displayHit("26");
+
+// view.displayMessage("Tap tap, is this thing on?");
